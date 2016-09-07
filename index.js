6935bf0508474b2099810aa8101a82a9a6a21c16
Olli Kankare <olli@usetrace.com>
2016-09-07 12:15:59 +0300

    Implement .all, .length, and .clear
    Bump up the version (and the jam)

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function EtcdStore(options) {
     var etcdOpts = options.etcdOptions || {};
     this.client = new Etcd(url, etcdOpts);
     this.ttl = options.ttl;
-    this.directory = options.directory || 'express-session';
+    this.directory = (options.directory || 'express-session') + '/';
 }
 util.inherits(EtcdStore, Store);
 
-EtcdStore.prototype.get = function (sid, callback) {
-    sid = this.directory + '/' + sid;
-    this.client.get(sid, function (err, data) {
+EtcdStore.prototype.get = function get(sid, callback) {
+    var key = this._getKeyFromId(sid);
+    this.client.get(key, function (err, data) {
         if (err && err.errorCode !== 100) return callback(err);
         if (!data || err && err.errorCode === 100) return callback(null, null);
         var response = validJson(data.node.value);
@@ -29,17 +29,47 @@ EtcdStore.prototype.get = function (sid, callback) {
     });
 };
 
-EtcdStore.prototype.set = function (sid, session, callback) {
-    sid = this.directory + '/' + sid;
+EtcdStore.prototype.set = function set(sid, session, callback) {
+    var key = this._getKeyFromId(sid);
     var ttl = this.ttl || ('number' == typeof session.cookie.maxAge ? session.cookie.maxAge / 1000 | 0 : oneDay);
-    return this.client.set(sid, JSON.stringify(session), { ttl: ttl }, callback);
+    var value = JSON.stringify(session);
+    return this.client.set(key, value, { ttl: ttl }, callback);
 };
-
 EtcdStore.prototype.touch = EtcdStore.prototype.set;
 
-EtcdStore.prototype.destroy = function (sid, callback) {
-    sid = this.directory + '/' + sid;
-    return this.client.del(sid, callback);
+EtcdStore.prototype.all = function all(callback) {
+    var key = this.directory;
+    this.client.get(key, {recursive: true}, function (err, listing) {
+        if (err) return callback(err);
+        if (!listing || !listing.node || !listing.node.nodes || !listing.node.nodes.length) return callback(null, []);
+        var sessions = listing.node.nodes.reduce(function (list, node) {
+            var val = validJson(node.value);
+            if (val) list.push(val);
+            return list;
+        }, []);
+        return callback(null, sessions);
+    });
+};
+
+EtcdStore.prototype.length = function length(callback) {
+    this.all(function (err, sessions) {
+        if (err) return callback(err);
+        return callback(null, sessions.length);
+    });
+};
+
+EtcdStore.prototype.clear = function clear(callback) {
+    var key = this.directory;
+    return this.client.del(key, { recursive: true }, callback);
+};
+
+EtcdStore.prototype.destroy = function destroy(sid, callback) {
+    var key = this._getKeyFromId(sid);
+    return this.client.del(key, callback);
+};
+
+EtcdStore.prototype._getKeyFromId = function _getKeyFromId(id) {
+    return this.directory + id;
 };
 
 module.exports = EtcdStore;
--- test.js
+++ test.js
@@ -1,20 +1,16 @@
 var assert = require('assert');
 var session = require('express-session');
 var EtcdStore = require('./');
-var store = new EtcdStore();
+var store = new EtcdStore({url: '192.168.99.100:32771', etcdOptions: { timeout: 1000 }});
 var insert = { cookie: { maxAge: 100000 }, name: 'olli' };
 var insertKey = '123';
 
 setAndCheck(insertKey, insert, function (err, data) {
-    assert.ok(!err, '#setAndCheck() got an error');
     assert.deepEqual(data, insert);
-    setAndCheck(insertKey, data, function (err, reData) {
-        assert.ok(!err, '#setAndCheck() got an error');
+    setAndCheckAll(insertKey, data, function (err, reData) {
         assert.deepEqual(reData, insert);
         touchAndCheck(insertKey, insert, function (err) {
-            assert.ok(!err, '#touchAndCheck() got an error');
             remove(insertKey, function (err) {
-                assert.ok(!err, '#remove() got an error');
                 console.log('all tests passed');
                 process.exit(0);
             });
@@ -28,7 +24,7 @@ function get(key, expected, callback) {
         expected = undefined;
     }
     store.get(key, function (err, data) {
-        assert.ok(!err, '#get() got an error');
+        assert.ifError(err)
         if (expected !== undefined) assert.deepEqual(expected, data);
         return callback(err, data);
     });
@@ -36,24 +32,42 @@ function get(key, expected, callback) {
 
 function setAndCheck(key, data, callback) {
     store.set(key, data, function (err, ok) {
-        assert.ok(!err, '#set() got an error');
+        assert.ifError(err)
         assert.ok(ok, '#set() is not ok');
         return get(key, data, callback);
     });
 }
 
+function setAndCheckAll(key, data, callback) {
+    setAndCheck(key, data, function (err) {
+        store.all(function (err, listing) {
+            assert.ifError(err)
+            var foundIndex = -1;
+            var ok = listing.some(function (item, i) {
+                var found = Object.keys(data).every(function (key) {
+                    return JSON.stringify(data[key]) === JSON.stringify(item[key]);
+                });
+                if (found) foundIndex = i;
+                return found;
+            });
+            assert.ok(ok, '#all() failed to deliver expected values');
+            return callback(err, listing[foundIndex]);
+        });
+    });
+}
+
 function touchAndCheck(key, data, callback) {
     store.touch(key, data, function (err) {
-        assert.ok(!err, '#touch() got an error');
+        assert.ifError(err)
         setTimeout(get, 10000, key, data, callback);
     });
 }
 
 function remove(key, callback) {
     store.destroy(key, function (err) {
-        assert.ok(!err, '#destroy() got an error');
+        assert.ifError(err)
         get(key, function (err, data) {
-            assert.ok(!err, '#get() got an error');
+            assert.ifError(err)
             assert.ok(!data, '#destroy() failed!');
             return callback();
         });
