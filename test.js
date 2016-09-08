056b5999ed560be51f60dfa281298a719fd341b2
Olli Kankare <olli@usetrace.com>
2016-09-08 18:15:18 +0300

    Recreate tests and update dev-deps accordingly. Bump the version too

--- test.js
+++ test.js
@@ -1,75 +1,61 @@
-var assert = require('assert');
-var session = require('express-session');
+var expect = require('chai').expect
 var EtcdStore = require('./');
-var store = new EtcdStore({url: '192.168.99.100:32771', etcdOptions: { timeout: 1000 }});
+// this requires an actual etcd server to test, so note that
+var store = new EtcdStore({ dir: 'express-etcd-test', etcdOptions: { timeout: 1000 } });
 var insert = { cookie: { maxAge: 100000 }, name: 'olli' };
 var insertKey = '123';
 
-setAndCheck(insertKey, insert, function (err, data) {
-    assert.deepEqual(data, insert);
-    setAndCheckAll(insertKey, data, function (err, reData) {
-        assert.deepEqual(reData, insert);
-        touchAndCheck(insertKey, insert, function (err) {
-            remove(insertKey, function (err) {
-                console.log('all tests passed');
-                process.exit(0);
+describe('EtcdStore', function () {
+    describe('#set()', function () {
+        it('should save without error', function (done) {
+            store.set(insertKey, insert, done);
+        });
+    });
+    describe('#get()', function () {
+        it('should return a session without an error', function (done) {
+            store.get(insertKey, function (err, data) {
+                expect(err).to.not.exist;
+                expect(data).to.deep.equal(insert);
+                return done();
             });
+        });
+    });
+    describe('#all()', function () {
+        this.slow(20000);
+        it('should return a list of all the sessions', function (done) {
+            store.all(function (err, listing) {
+                expect(err).to.not.exist;
+                expect(listing).to.exist;
+                var foundIndex = -1;
+                var ok = listing.some(function (item, i) {
+                    var found = Object.keys(insert).every(function (key) {
+                        return JSON.stringify(insert[key]) === JSON.stringify(item[key]);
+                    });
+                    if (found) foundIndex = i;
+                    return found;
+                });
+                expect(ok).to.be.ok;
+                done();
+            });
+        });
+    });
+    describe('#length()', function () {
+        it('return a length without an error', function (done) {
+            store.length(function (err, len) {
+                expect(err).to.not.exist;
+                expect(len).to.be.a('number');
+                done();
+            });
+        });
+    });
+    describe('#destroy()', function () {
+        it('should destroy sessiondata without error', function (done) {
+            store.destroy(insertKey, done);
+        });
+    });
+    describe('#clear()', function () {
+        it('should clear all sessiondata without error', function (done) {
+            store.clear(done);
         });
     });
 });
-
-function get(key, expected, callback) {
-    if ('function' === typeof expected) {
-        callback = expected;
-        expected = undefined;
-    }
-    store.get(key, function (err, data) {
-        assert.ifError(err)
-        if (expected !== undefined) assert.deepEqual(expected, data);
-        return callback(err, data);
-    });
-}
-
-function setAndCheck(key, data, callback) {
-    store.set(key, data, function (err, ok) {
-        assert.ifError(err)
-        assert.ok(ok, '#set() is not ok');
-        return get(key, data, callback);
-    });
-}
-
-function setAndCheckAll(key, data, callback) {
-    setAndCheck(key, data, function (err) {
-        store.all(function (err, listing) {
-            assert.ifError(err)
-            var foundIndex = -1;
-            var ok = listing.some(function (item, i) {
-                var found = Object.keys(data).every(function (key) {
-                    return JSON.stringify(data[key]) === JSON.stringify(item[key]);
-                });
-                if (found) foundIndex = i;
-                return found;
-            });
-            assert.ok(ok, '#all() failed to deliver expected values');
-            return callback(err, listing[foundIndex]);
-        });
-    });
-}
-
-function touchAndCheck(key, data, callback) {
-    store.touch(key, data, function (err) {
-        assert.ifError(err)
-        setTimeout(get, 10000, key, data, callback);
-    });
-}
-
-function remove(key, callback) {
-    store.destroy(key, function (err) {
-        assert.ifError(err)
-        get(key, function (err, data) {
-            assert.ifError(err)
-            assert.ok(!data, '#destroy() failed!');
-            return callback();
-        });
-    });
-}
